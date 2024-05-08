133dacecff5ee4f8a758929f7c1ca0100c934ede
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-08 13:53:38 +0100

    Fetch Single Property & PropertyHeaderImage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import connectDB from '@/config/db';
+import Property from '@/models/Property';
+import { PropertyModel } from '@/models/Property';
+import { convertToSerializeableObject } from '@/utils/convertToObject';
 
-const PropertyPage = () => {
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+
+import PropertyHeaderImage from '@/components/Properties/PropertyHeaderImage';
+
+const PropertyPage = async ({ params }: { params: { id: string } }) => {
+  await connectDB();
+
+  const propertyDoc: PropertyModel | null = await Property.findById(params.id).lean();
+
+  if (!propertyDoc) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    );
+  }
+
+  const property = convertToSerializeableObject(propertyDoc) as PropertyModel;
+
   return (
-    <div>PropertyPage</div>
+    <>
+      <PropertyHeaderImage image={ property.images[0] } />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' />Back to Properties
+          </Link>
+        </div>
+      </section>
+    </>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,12 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
