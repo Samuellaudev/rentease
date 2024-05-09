4b49a1cce007b6624bf3d0e8965730dd4523de7b
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-09 10:24:35 +0100

    Property Details Component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 
 import PropertyHeaderImage from '@/components/Properties/PropertyHeaderImage';
+import PropertyDetails from '@/components/Properties/PropertyDetails';
 
 const PropertyPage = async ({ params }: { params: { id: string } }) => {
   await connectDB();
@@ -34,6 +35,18 @@ const PropertyPage = async ({ params }: { params: { id: string } }) => {
           >
             <FaArrowLeft className='mr-2' />Back to Properties
           </Link>
+        </div>
+      </section>
+      <section className='bg-blue-50'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            <PropertyDetails property={ property } />
+            {/* <aside className='space-y-4'>
+              <BookmarkButton property={property} />
+              <ShareButtons property={ property } PUBLIC_DOMAIN={ PUBLIC_DOMAIN } />
+              <PropertyContactForm property={property} />
+            </aside> */}
+          </div>
         </div>
       </section>
     </>
