51486c93305e76f875c11553acede3cf873bea2f
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-13 22:06:03 +0100

    Properties Component Refactor

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -2,8 +2,8 @@ import connectDB from '@/config/db';
 import Property from '@/models/Property';
 import { PropertyType } from '@/types/property.type';
 
-import PropertyCard from '@/components/Properties/PropertyCard';
 import PropertySearchForm from '@/components/Properties/PropertySearchForm'
+import Properties from '@/components/Properties/Properties';
 
 const PropertiesPage = async () => {
   await connectDB()
@@ -17,19 +17,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto px-4 py-6'>
-          { properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-              { properties.map((property) => (
-                <PropertyCard key={ property._id } property={ property } />
-              )) }
-            </div>
-          ) }
-        </div>
-      </section>
+      <Properties properties={ properties }/>
     </>
   );
 };
---
+++ components/Properties/Properties.tsx
@@ -0,0 +1,26 @@
+import { PropertyType } from '@/types/property.type';
+import PropertyCard from '@/components/Properties/PropertyCard';
+
+interface PropertiesProps {
+  properties: PropertyType[]
+}
+
+const Properties = ({ properties }: PropertiesProps) => {
+  return (
+    <section className='px-4 py-6'>
+    <div className='container-xl lg:container m-auto px-4 py-6'>
+      { properties.length === 0 ? (
+        <p>No properties found</p>
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          { properties.map((property) => (
+            <PropertyCard key={ property._id } property={ property } />
+          )) }
+        </div>
+      ) }
+    </div>
+  </section>
+  )
+}
+
+export default Properties
