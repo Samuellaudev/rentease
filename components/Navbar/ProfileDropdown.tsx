4550c6be6d541ab6f73b2188a89f5f3f66821cac
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-17 13:20:31 +0100

    Redirect user to homepage after logout

--- components/Navbar/ProfileDropdown.tsx
+++ components/Navbar/ProfileDropdown.tsx
@@ -14,9 +14,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+interface CallbackUrl {
+  callbackUrl: string
+}
 interface ProfileDropdownProps {
   profileImage: string
-  signOut: () => void
+  signOut: ({ callbackUrl }: CallbackUrl) => void
 }
 
 const ProfileDropdown = ({
@@ -102,7 +105,7 @@ const ProfileDropdown = ({
               id='user-menu-item-2'
               onClick={ () => {
                 setIsProfileMenuOpen(false);
-                signOut();
+                signOut({ callbackUrl: '/' });
               } }
             >
               Sign Out
@@ -111,74 +114,6 @@ const ProfileDropdown = ({
         </DropdownMenuContent>
       ) }
     </DropdownMenu>
-    // <div className='relative ml-3'>
-    //   <div>
-    //     <button
-    //       type='button'
-    //       className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-    //       id='user-menu-button'
-    //       aria-expanded='false'
-    //       aria-haspopup='true'
-    //       onClick={ () => setIsProfileMenuOpen((prev) => !prev) }
-    //     >
-    //       <span className='absolute -inset-1.5'></span>
-    //       <span className='sr-only'>Open user menu</span>
-    //       <Image
-    //         className='h-8 w-8 rounded-full'
-    //         src={ profileImage || profileDefault }
-    //         width={ 40 }
-    //         height={ 40 }
-    //         alt='Profile Image'
-    //       />
-    //     </button>
-    //   </div>
-
-    //   {/* <!-- Profile dropdown --> */ }
-    //   { isProfileMenuOpen && (
-    //     <div
-    //       id='user-menu'
-    //       className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-    //       role='menu'
-    //       aria-orientation='vertical'
-    //       aria-labelledby='user-menu-button'
-    //       tabIndex={ -1 }
-    //     >
-    //       <Link
-    //         href='/profile'
-    //         className='block px-4 py-2 text-sm text-gray-700'
-    //         role='menuitem'
-    //         tabIndex={ -1 }
-    //         id='user-menu-item-0'
-    //         onClick={ () => setIsProfileMenuOpen(false) }
-    //       >
-    //         Your Profile
-    //       </Link>
-    //       <Link
-    //         href='/properties/saved'
-    //         className='block px-4 py-2 text-sm text-gray-700'
-    //         role='menuitem'
-    //         tabIndex={ -1 }
-    //         id='user-menu-item-1'
-    //         onClick={ () => setIsProfileMenuOpen(false) }
-    //       >
-    //         Saved Properties
-    //       </Link>
-    //       <Link
-    //         href='#'
-    //         className='block px-4 py-2 text-sm text-gray-700'
-    //         role='menuitem'
-    //         tabIndex={ -1 }
-    //         id='user-menu-item-2'
-    //         onClick={ () => {
-    //           setIsProfileMenuOpen(false);
-    //           signOut();
-    //         } }
-    //       >
-    //         Sign Out
-    //       </Link>
-    //     </div>
-    //   ) }
-    // </div>
   )
 }
 
