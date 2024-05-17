bf34d41140f6a48cb9bc991ca466c294bb766a45
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-17 13:19:05 +0100

    Modified BookmarkButton using shadcn/ui

--- components/Properties/BookmarkButton.tsx
+++ components/Properties/BookmarkButton.tsx
@@ -9,6 +9,7 @@ import { PropertyType } from '@/types/property.type';
 
 import { toast } from 'react-toastify';
 import { FaBookmark } from 'react-icons/fa';
+import { Button } from '../ui/button';
 
 interface PropertyProps {
   property: PropertyType
@@ -50,19 +51,19 @@ const BookmarkButton = ({ property }: PropertyProps) => {
   if (loading) return <p className='text-center'>Loading...</p>;
 
   return isBookmarked ? (
-    <button
+    <Button
       onClick={handleClick}
-      className='bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+      className='bg-red-500 hover:bg-red-600 text-white text-md font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
     >
       <FaBookmark className='mr-2' /> Remove Bookmark
-    </button>
+    </Button>
   ) : (
-    <button
+    <Button
       onClick={handleClick}
-      className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+      className='bg-primary hover:bg-cyan-600 text-white text-md font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
     >
       <FaBookmark className='mr-2' /> Bookmark Property
-    </button>
+    </Button>
   );
 };
 
