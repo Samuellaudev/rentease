3dd89a6a5c98f60c5b75ae5a2180c78310af1936
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-05-17 13:20:06 +0100

    Fixed not showing correct image in PropertyImage

--- components/Properties/PropertyImages.tsx
+++ components/Properties/PropertyImages.tsx
@@ -42,8 +42,8 @@ const PropertyImages = ({ images }: Images) => {
                     }` }
                 >
                   <Item
-                    original={ images[0] }
-                    thumbnail={ images[0] }
+                    original={ image }
+                    thumbnail={ image }
                     width='1000'
                     height='600'
                   >
